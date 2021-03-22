a2265ea06a25f508aa2767a2c2bcf8535f3b2b54
James Jordan <jpjordan1987@gmail.com>
2021-03-22 19:29:26 -0400

    mapping out models

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
+//Done, not tested
 
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
@@ -8,7 +9,7 @@ const sequelize = process.env.JAWSDB_URL
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
-        decimalNumbers: true,
+        decimalNumbers: true,  //do I need a port??
       },
     });
 
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,14 +7,27 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    category_id: {
+      type:DataTypes.INTEGER,
+      references:{
+        model:'product',
+        key:'id'
+      }
   },
+},
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
-);
+  },
+
+  );
 
 module.exports = ProductTag;
